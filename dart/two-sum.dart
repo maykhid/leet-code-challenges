45519f395bae8f0914cd3d2aca74e887f7173e85
Henry Ifebunandu <henryifebunandu@gmail.com>
2022-05-17 07:57:23 +0100

    refactor: two-sum.dart

--- dart/two-sum.dart
+++ dart/two-sum.dart
@@ -24,8 +24,8 @@ void main() {
 List<int> twoSum(List<int> nums, int target) {
   int firstNum = 0, secondNum = 0;
   
-  for(int i = 0; i <= nums.length; i++) {
-    for(int j = i + 1; j <= nums.length - 1; j++) {
+  for(int i = 0; i < nums.length; i++) {
+    for(int j = i + 1; j < nums.length; j++) {
       if(nums[i] + nums[j] == target) {
         firstNum = i;
         secondNum = j;
